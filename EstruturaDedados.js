8aa597da9376fe3f662879d667ae3a2ab358298b
SperandioR <102339706+SperandioR@users.noreply.github.com>
2022-04-18 14:45:20 -0300

    Create EstruturaDedados.js

---
+++ EstruturaDedados.js
@@ -0,0 +1,12 @@
+var diasDaSemana = [
+    'Segunda -feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
+    'Domingo'
+    
+];
+
+console.log(diasDaSemana[2]);
